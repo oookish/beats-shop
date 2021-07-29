238555db1a4089f7e293e9e50552c9e3cc235f81
Golub Eduard <sonofmisery@inbox.ru>
2021-07-30 02:55:48 +0500

    Сделал выпадающее меню в секции покупок

--- JS/slider.js
+++ JS/slider.js
@@ -1,36 +1,36 @@
-// const left = document.querySelector('#left');
-// const right = document.querySelector('#right');
-// const itemsList = document.querySelector('#items');
-// const computedStyles = window.getComputedStyle(itemsList);
-// const items = document.querySelectorAll('.slider-item');
+const left = document.querySelector('#left');
+const right = document.querySelector('#right');
+const itemsList = document.querySelector('#items');
+const computedStyles = window.getComputedStyle(itemsList);
+const items = document.querySelectorAll('.slider-item');
 
 
-// const minRight = 0;
-// const itemWidth = getComputedStyle(items[0]).width;
-// const step = parseInt(itemWidth);
-// const preShowItems = 930 / step;
-// const maxRight = (items.length - preShowItems) * step;
-// let currentRight = 0;
+const minRight = 0;
+const itemWidth = getComputedStyle(items[0]).width;
+const step = parseInt(itemWidth);
+const preShowItems = 930 / step;
+const maxRight = (items.length - preShowItems) * step;
+let currentRight = 0;
 
-// itemsList.style.right = currentRight;
+itemsList.style.right = currentRight;
 
-// right.addEventListener('click', e => {
-//   e.preventDefault();
+right.addEventListener('click', e => {
+  e.preventDefault();
 
-//   if (currentRight < maxRight) {
-//     currentRight += step;
-//     itemsList.style.right = `${currentRight}px`;
-//   }
-// })
+  if (currentRight < maxRight) {
+    currentRight += step;
+    itemsList.style.right = `${currentRight}px`;
+  }
+})
 
-// left.addEventListener('click', e => {
-//   e.preventDefault();
+left.addEventListener('click', e => {
+  e.preventDefault();
 
-//   if (currentRight > minRight) {
-//     currentRight -= step;
-//     itemsList.style.right = `${currentRight}px`;
-//   }
-// })
+  if (currentRight > minRight) {
+    currentRight -= step;
+    itemsList.style.right = `${currentRight}px`;
+  }
+})
 
 
 ///////////////////////////////////////////////////////// ВТОРОЙ ВАРИАНТ /////////////////////////////////////////////////////////
